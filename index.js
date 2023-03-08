f02cd78aaaea8ff1269590ee5e1c027db98f6436
flo-tmp <127289598+flo-tmp@users.noreply.github.com>
2023-03-08 08:56:06 +0100

    go

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+const http = require("http");
+const {main} = require('./script');
+
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+
+    const data = main();
+
+    res.write(data);
     res.end();
-}).listen(process.env.PORT || 3000);
+  })
+  .listen(process.env.PORT || 3000);
---
+++ script.js
@@ -0,0 +1,5 @@
+function main() {
+  return "Bonjour";
+}
+
+module.exports = { main }
